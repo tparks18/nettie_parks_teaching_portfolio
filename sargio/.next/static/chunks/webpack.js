b494f2310f876c2e3616217aa2677bc4b223843f
Taty <tatyanaparks@gmail.com>
2023-06-22 10:49:52 -0500

    changed contact section to include location and email

--- sargio/.next/static/chunks/webpack.js
+++ sargio/.next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "8fedce006cec014c"; }
+/******/ 		__webpack_require__.h = function() { return "0b513646517f61f8"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- sargio/src/components/Contact.js
+++ sargio/src/components/Contact.js
@@ -57,18 +57,14 @@ const Contact = () => {
           <h3>Get in touch.</h3>
         </div>
         <div className="row">
-          <div className="col-lg-5 col-xl-4 m-15px-tb">
+          <div className="col-lg-7 col-xl-8 m-15px-tb">
             <div className="contact-info">
               <h4>I would love to help you! Get in touch</h4>
-              <p>
-                Always available for tutoring. Feel free to contact me.
-              </p>
+              <p>Always available for tutoring. Feel free to contact me.</p>
               <ul>
                 <li className="media">
                   <i className="ti-map" />
-                  <span className="media-body">
-                    Chicago, IL.
-                  </span>
+                  <span className="media-body">Chicago, IL.</span>
                 </li>
                 <li className="media">
                   <i className="ti-email" />
@@ -79,110 +75,6 @@ const Contact = () => {
                   <span className="media-body">+044 9696 9696 3636</span>
                 </li> */}
               </ul>
-            </div>
-          </div>
-          <div className="col-lg-7 col-xl-8 m-15px-tb">
-            <div className="contact-form">
-              <h4>Say Something</h4>
-              <form id="contact-form" onSubmit={(e) => onSubmit(e)}>
-                <div className="row">
-                  <div className="col-md-6">
-                    <div className="form-group">
-                      <input
-                        name="name"
-                        onChange={(e) => onChange(e)}
-                        value={name}
-                        id="name"
-                        placeholder="Name *"
-                        className={`form-control ${
-                          error ? (!name ? "invalid" : "") : ""
-                        }`}
-                        type="text"
-                      />
-                    </div>
-                  </div>
-                  <div className="col-md-6">
-                    <div className="form-group">
-                      <input
-                        name="email"
-                        onChange={(e) => onChange(e)}
-                        value={email}
-                        id="email"
-                        placeholder="Email *"
-                        className={`form-control ${
-                          error ? (!email ? "invalid" : "") : ""
-                        }`}
-                        type="email"
-                      />
-                    </div>
-                  </div>
-                  <div className="col-12">
-                    <div className="form-group">
-                      <input
-                        name="subject"
-                        onChange={(e) => onChange(e)}
-                        value={subject}
-                        id="subject"
-                        placeholder="Subject *"
-                        className={`form-control ${
-                          error ? (!subject ? "invalid" : "") : ""
-                        }`}
-                        type="text"
-                      />
-                    </div>
-                  </div>
-                  <div className="col-md-12">
-                    <div className="form-group">
-                      <textarea
-                        name="message"
-                        onChange={(e) => onChange(e)}
-                        value={message}
-                        id="message"
-                        placeholder="Your message *"
-                        rows={5}
-                        className={`form-control ${
-                          error ? (!message ? "invalid" : "") : ""
-                        }`}
-                      />
-                    </div>
-                  </div>
-                  <div className="col-md-12">
-                    <div className="send">
-                      {/* <button
-                        onSubmit={(e) => onSubmit(e)}
-                        className="px-btn px-btn-theme"
-                        type="button"
-                        value="Send"
-                      >
-                        {" "}
-                        send message
-                      </button> */}
-                      <input
-                        className="px-btn px-btn-theme"
-                        type="submit"
-                        value="send message"
-                      />
-                    </div>
-                    <span
-                      id="suce_message"
-                      className="text-success"
-                      style={{
-                        display:
-                          error !== null ? (!error ? "block" : "none") : "none",
-                      }}
-                    >
-                      Message Sent Successfully
-                    </span>
-                    <span
-                      id="err_message"
-                      className="text-danger"
-                      style={{ display: "none" }}
-                    >
-                      Message Sending Failed
-                    </span>
-                  </div>
-                </div>
-              </form>
             </div>
           </div>
           <div className="col-12">
