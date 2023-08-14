9de3b47eaa9d48f2e338895d94e85d3b0a3384b0
Taty <tatyanaparks@gmail.com>
2023-08-14 09:45:29 -0500

    changed margins

--- src/components/Services.js
+++ src/components/Services.js
@@ -19,15 +19,15 @@ const Services = () => {
           <div className="col-sm-6 m-15px-tb">
             <div className="feature-box-01 media">
               <i className="icon theme-bg ti-stats-up" />
-              <div className="feature-content media-body">
+              <div
+                className="feature-content media-body"
+                style={{ minHeight: "200px" }}
+              >
                 <h5>Math</h5>
                 <p>
                   I can assist students with math topics such as Common Core
                   Math, Number Sense, Problem Solving, Interpreting Data,
                   Fractions and Decimals, and Geometry.
-                  <br />
-                  <br />
-                  <br />
                 </p>
               </div>
             </div>
@@ -35,16 +35,14 @@ const Services = () => {
           <div className="col-sm-6 m-15px-tb">
             <div className="feature-box-01 media">
               <i className="icon theme-bg ti-flag" />
-              <div className="feature-content media-body">
+              <div
+                className="feature-content media-body"
+                style={{ minHeight: "200px" }}
+              >
                 <h5>Regular Test Preparation</h5>
                 <p>
                   I can assist students with study skills and give them
                   strategies to prepare for reading and math tests.
-                  <br />
-                  <br />
-                  <br />
-                  <br />
-                  <br />
                 </p>
               </div>
             </div>
@@ -52,7 +50,10 @@ const Services = () => {
           <div className="col-sm-6 m-15px-tb">
             <div className="feature-box-01 media">
               <i className="icon theme-bg ti-write" />
-              <div className="feature-content media-body">
+              <div
+                className="feature-content media-body"
+                style={{ minHeight: "200px" }}
+              >
                 <h5>Writing</h5>
                 <p>
                   I can help students with sentence construction, proper
@@ -67,16 +68,15 @@ const Services = () => {
           <div className="col-sm-6 m-15px-tb">
             <div className="feature-box-01 media">
               <i className="icon theme-bg ti-book" />
-              <div className="feature-content media-body">
+              <div
+                className="feature-content media-body"
+                style={{ minHeight: "200px" }}
+              >
                 <h5>English</h5>
                 <p>
                   I can assist students with Literature, Informational Text,
                   Grammar, Foundational Skills, Reading Fluency, and
                   Comprehension.
-                  <br />
-                  <br />
-                  <br />
-                
                 </p>
               </div>
             </div>
@@ -84,15 +84,14 @@ const Services = () => {
           <div className="col-sm-6 m-15px-tb">
             <div className="feature-box-01 media">
               <i className="icon theme-bg ti-rocket" />
-              <div className="feature-content media-body">
+              <div
+                className="feature-content media-body"
+                style={{ minHeight: "200px" }}
+              >
                 <h5>Science</h5>
                 <p>
                   I can assist students with understanding Forces, Life Science,
                   Environments and Survival, Weather, and Climate.
-                  <br />
-                  <br />
-                  <br />
-                  <br />
                 </p>
               </div>
             </div>
@@ -100,16 +99,15 @@ const Services = () => {
           <div className="col-sm-6 m-15px-tb">
             <div className="feature-box-01 media">
               <i className="icon theme-bg ti-world" />
-              <div className="feature-content media-body">
+              <div
+                className="feature-content media-body"
+                style={{ minHeight: "200px" }}
+              >
                 <h5>Social Studies</h5>
-                <p className='mb-53'>
+                <p className="mb-4">
                   I can assist students with social studies topics such as
                   Evaluating Sources and Using Evidence, Civic and Political
                   Institutions, and Geography.
-                  <br />
-                  <br />
-                  <br /> 
-                  <br /> 
                 </p>
               </div>
             </div>
